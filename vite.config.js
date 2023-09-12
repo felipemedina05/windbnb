ee8d728d183f44ce29721da43ba520945cc16472
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-09-11 21:05:57 -0500

    corrigiendo pages en blanco

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bases:"https://felipemedina05.github.io/windbnb/"
+  bases:"/windbnb"
 
 })
